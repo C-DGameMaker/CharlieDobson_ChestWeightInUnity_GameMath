cdd2bb001f724dffd6ded375455e1b0838f75a81
C-DGameMaker <charlotte5634@outlook.com>
2025-10-27 11:11:18 -0300

    Added Ability to press spacebar
    I made sure it worked first

--- Assets/ChestWeight.cs
+++ Assets/ChestWeight.cs
@@ -1,6 +1,18 @@
+using Mono.Cecil;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ChestWeight : MonoBehaviour
 {
+    [Header("CHEST TYPES")]
+    public int wood = 1;
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("You pressed the spaceBar");
+        }
+    
+    }
     
 }
