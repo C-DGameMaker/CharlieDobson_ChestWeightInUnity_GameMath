d03bc80149d1ef9413ad0551d0d2cdfb026465c4
C-DGameMaker <charlotte5634@outlook.com>
2025-10-27 11:20:58 -0300

    All headers added

--- Assets/ChestWeight.cs
+++ Assets/ChestWeight.cs
@@ -6,6 +6,23 @@ public class ChestWeight : MonoBehaviour
 {
     [Header("CHEST TYPES")]
     public int wood = 1;
+
+    [Header("WOOD CHEST DROPS")]
+    public int woodCommon = 1;
+
+    [Header("BRONZE CHEST DROPS")]
+    public int bronzeCommon = 1;
+
+    [Header("STONE CHEST DROPS")]
+    public int stoneCommon = 1;
+
+    [Header("GOLD CHEST DROPS")]
+    public int goldCommon = 1;
+
+    [Header("PLATIUM CHEST DROPS")]
+    public int platiumCommon = 1;
+
+
     private void Update()
     {
         if(Input.GetKey(KeyCode.Space))
