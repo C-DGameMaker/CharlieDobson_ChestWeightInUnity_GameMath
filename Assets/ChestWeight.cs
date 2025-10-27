67cbf198c46acb4c9773d6b91b0988ef6aa8a754
C-DGameMaker <charlotte5634@outlook.com>
2025-10-27 12:12:33 -0300

    Made Some tables
    Currently tryna figure out how for loops work to add stuff together to get a certain outcome, but I have some other things set up for it.

--- Assets/ChestWeight.cs
+++ Assets/ChestWeight.cs
@@ -1,35 +1,110 @@
 using Mono.Cecil;
+using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor;
+using UnityEditor.Search;
 using UnityEngine;
 
 public class ChestWeight : MonoBehaviour
 {
     [Header("CHEST TYPES")]
-    public int wood = 1;
+    [SerializeField] int woodChest;
+    [SerializeField] int bronzeChest;
+    [SerializeField] int silverChest;
+    [SerializeField] int goldChest;
+    [SerializeField] int platinumChest;
+    static int totalChest;
+    
 
     [Header("WOOD CHEST DROPS")]
-    public int woodCommon = 1;
+    [SerializeField] int woodCommon;
+    [SerializeField] int woodUnCommon;
+    [SerializeField] int woodRare;
+    [SerializeField] int woodEpic;
+    [SerializeField] int woodLegandary;
+    static int woodChance;
 
     [Header("BRONZE CHEST DROPS")]
-    public int bronzeCommon = 1;
+    [SerializeField] int bronzeCommon;
+    [SerializeField] int bronzeUnCommon;
+    [SerializeField] int bronzeRare;
+    [SerializeField] int bronzeEpic;
+    [SerializeField] int bronzeLegandary;
+    static int bronzeChance;
 
-    [Header("STONE CHEST DROPS")]
-    public int stoneCommon = 1;
+    [Header("SILVER CHEST DROPS")]
+    [SerializeField] int silverCommon;
+    [SerializeField] int silverUnCommon;
+    [SerializeField] int silverRare;
+    [SerializeField] int silverEpic;
+    [SerializeField] int silverLegandary;
+    static int silverChance;
 
     [Header("GOLD CHEST DROPS")]
-    public int goldCommon = 1;
+    [SerializeField] int goldCommon;
+    [SerializeField] int goldUnCommon;
+    [SerializeField] int goldRare;
+    [SerializeField] int goldEpic;
+    [SerializeField] int goldLegandary;
+    static int goldChance;
 
-    [Header("PLATIUM CHEST DROPS")]
-    public int platiumCommon = 1;
+    [Header("PLATINUM CHEST DROPS")]
+    [SerializeField] int platinumCommon;
+    [SerializeField] int platinumUnCommon;
+    [SerializeField] int platinumRare;
+    [SerializeField] int platinumEpic;
+    [SerializeField] int platinumLegandary;
+    static int platChance;
 
+    static List<int> chests = new List<int> { };
+    static List<int> wood = new List<int> { };
+    static List<int> bronze = new List<int> { };
+    static List<int> silver = new List<int> { };
+    static List<int> gold = new List<int> { };
+    static List<int> platinum = new List<int> { };
 
-    private void Update()
+    void Update()
     {
-        if(Input.GetKey(KeyCode.Space))
+        totalChest = woodChest + bronzeChest + silverChest + goldChest + platinumChest;
+        if (Input.GetKey(KeyCode.Space))
         {
-            Debug.Log("You pressed the spaceBar");
+            int chest = Random.Range(0,(totalChest + 1));
+
+            for (int i = 0; i < chest; i++)
+            {
+
+            }
+
         }
-    
     }
-    
+
+    void WoodChest()
+    {
+        woodChance = woodCommon + woodUnCommon + woodRare + woodEpic + woodLegandary;
+
+        int chest = Random.Range(0, (woodChance + 1));
+    }
+
+    void BronzeChest()
+    {
+
+    }
+
+    void SilverChest()
+    {
+
+    }
+
+    void GoldChest()
+    {
+
+    }
+
+    void PlatinumChest()
+    {
+
+    }
+
+
+
 }
